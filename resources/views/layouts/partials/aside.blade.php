e84570d2c125f01062c40736ea765db580897477
DiegoHV <diegohurtado2412@gmail.com>
2021-09-24 20:25:45 -0400

    cambios

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -3,7 +3,7 @@
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('vendor/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">TE AYUDO</span>
     </a>
 
     <!-- Sidebar -->
@@ -14,7 +14,7 @@
                 <img src="{{ asset('vendor/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="/perfil" class="d-block">Alexander Pierce</a>
+                <a href="/perfil" class="d-block">Mi Perfil</a>
             </div>
         </div>
 
@@ -40,6 +40,24 @@
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             CATEGORIAS
+                            <span class="right badge badge-danger">New</span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/nose" class="nav-link">
+                        <i class="nav-icon fas fa-star"></i>
+                        <p>
+                            FAVORITOS
+                            <span class="right badge badge-danger">New</span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/nose" class="nav-link">
+                        <i class="nav-icon fas fa-medal"></i>
+                        <p>
+                            RECOMENDADOS
                             <span class="right badge badge-danger">New</span>
                         </p>
                     </a>
